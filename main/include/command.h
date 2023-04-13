38d37f31ad1b85a72bee708f2482645439670a78
mzakocs <mzakocs@gmail.com>
2023-04-13 14:53:46 -0700

    Started on command system

--- main/include/command.h
+++ main/include/command.h
@@ -8,6 +8,14 @@
 #include <task.h>
 #include <semphr.h>
 #include "pico/stdlib.h"
+#include "gse.h"
+
+#define COMMAND_MAX_QUEUE_ITEMS 64
+
+typedef char* command_message_type;
+QueueHandle_t command_queue;
+
+void parse_inbound_command(command_message_type command);
 
 // Main Task
 void command_task(void* unused_arg);
--- main/src/command.c
+++ main/src/command.c
@@ -1,8 +1,14 @@
 #include "command.h"
 
+void queue_inbound_command
+
 void command_task(void* unused_arg) {
     // Initialize command queue
+    command_queue = xQueueCreate(COMMAND_MAX_QUEUE_ITEMS, sizeof(command_message_type));
+    command_message_type command = NULL;
     while (true) {
-
+        // Wait on a message in the queue
+        xQueueReceive(command_queue, &command, UART_QUEUE_CHECK_TIME);
+        // TODO: Process command and send response
     }
 }
