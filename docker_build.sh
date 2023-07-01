16b5eefc90757037552d80ff6a266f3f17a099f5
mzakocs <mzakocs@gmail.com>
2023-06-30 20:24:46 -0700

    Fixed docker build

--- docker_build.sh
+++ docker_build.sh
@@ -1,8 +1,10 @@
 # Build firmware inside the container
-docker build . -t coconut_fsw --progress=plain
+docker build . -t coconut_fsw
 
 # Copy files out of the container
-mkdir docker_build
-docker cp coconut_fsw:/coconut-fsw/build/main/COCONUTFSW.uf2 ./docker_build/COCONUTFSW.uf2
+mkdir -p docker_build
 
-#
+docker run --name coconut_build -id coconut_fsw
+docker cp coconut_build:/coconut-fsw/build/main/COCONUTFSW.uf2 ./docker_build/COCONUTFSW.uf2
+docker kill coconut_build
+docker rm coconut_build
