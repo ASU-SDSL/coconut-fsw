0a0b6863e4d6712c74e075fe1f44c0b77fc5a130
mzakocs <mzakocs@gmail.com>
2023-08-25 14:06:45 -0700

    Fixes to spacepacket testing script

--- main/testing/ccsds_cmd.py
+++ main/testing/ccsds_cmd.py
@@ -4,18 +4,19 @@ import serial
 # Initialize serial port
 # ttyUSB0 for cmd/tlm spacepackets bytes
 # ttyACM0 (usb-c) for logging messages
-ser = serial.Serial('/dev/ttyUSB0')
+ser = serial.Serial('/dev/ttyUSB0', baudrate=115200)
 
 # Create spacepacket
 spacepacket_header = SpacePacketHeader(
-    packet_type=PacketType.TC, apid=0x01, seq_count=0, data_len=0
+    packet_type=PacketType.TC, apid=0x01, seq_count=0, data_len=5
 )
 header_bytes = spacepacket_header.pack()
 
 # Create full payload
-payload = header_bytes
-payload += "A" * 69
-payload += "\x00"
+payload = b"\x35\x2E\xF8\x53"
+payload += header_bytes
+payload += b"WOW!"
+payload += b"\x00"
 
 # Send spacepacket uart port
 ser.write(payload)
