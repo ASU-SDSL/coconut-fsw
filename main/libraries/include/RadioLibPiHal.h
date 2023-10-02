2facf313380da93f6e84195911b3a6ed6769c134
Jonathan Franco <97241584+jonathanfranco1@users.noreply.github.com>
2023-10-01 23:29:23 -0700

    Updated RadioLibPiHal.h

--- main/libraries/include/RadioLibPiHal.h
+++ main/libraries/include/RadioLibPiHal.h
@@ -1,143 +1,131 @@
-/*#include <RadioLib.h>
+#include <RadioLib.h>
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/adc.h"
 #include "hardware/spi.h"
 
-class PiHal : public RadioLibHal {
-    public:
-        // speed taken from radio lib pi example
-        PiHal(uint32_t spiSpeed = 2000000)
-            : RadioLibHal(0, 1, 0, 1, 0, 1), // PI_INPUT, PI_OUTPUT, PI_LOW, PI_HIGH, RISING_EDGE, FALLING_EDGE - taken from pigpio.h, I am assuming it will be the same
-            _spiSpeed(spiSpeed) {
+
+class PiPicoHal : public RadioLibHal {
+public:
+    PiPicoHal(spi_inst_t *spi, uint32_t spi_speed = 2000000)
+        : RadioLibHal(0, 1, 0, 1, 0, 1),  
+          _spi(spi),
+          _spi_speed(spi_speed) {
+    }
+
+    void init() override {
+        stdio_init_all();
+
+        spiBegin();
+
+        gpio_init(18);
+        gpio_set_dir(18, GPIO_OUT);
+        gpio_put(18, 1);
+    }
+
+    void term() override {
+        spiEnd();
+
+        gpio_init(18);
+        gpio_set_dir(18, GPIO_OUT);
+        gpio_put(18, 0);
+    }
+
+    void pinMode(uint32_t pin, uint32_t mode) override {
+        if (pin == RADIOLIB_NC) {
+            return;
+        }
+        gpio_init(pin);
+        gpio_set_dir(pin, mode);
+    }
+
+    void digitalWrite(uint32_t pin, uint32_t value) override {
+        if (pin == RADIOLIB_NC) {
+            return;
+        }
+        gpio_put(pin, value);
+    }
+
+    uint32_t digitalRead(uint32_t pin) override {
+        if (pin == RADIOLIB_NC) {
+            return 0;
+        }
+        return gpio_get(pin);
+    }
+    typedef void(* gpio_irq_callback_t) (uint gpio, uint32_t event_mask);
+    void gpio_set_irq_enabled_with_callback (uint gpio, uint32_t event_mask, bool enabled, gpio_irq_callback_t callback);
+    void attachInterrupt(uint32_t interruptNum, void (*interruptCb)(void), uint32_t mode) override {
+        if (interruptNum == RADIOLIB_NC) {
+            return;
         }
 
-        void init() override {
-            // Enable SPI using the function defined later in this class
-            spiBegin();
+        // Setting the interrupt with the Raspberry Pi Pico SDK
+        gpio_set_irq_enabled_with_callback(interruptNum, mode, true, (gpio_irq_callback_t)interruptCb);
+    }
 
-            // anything else we need to do to initialize the radio?
+    void detachInterrupt(uint32_t interruptNum) override {
+        if (interruptNum == RADIOLIB_NC) {
+            return;
         }
 
-        // stop the spi
-        void term() override {
-            // Disable spi using function defined later in this class
-            spiEnd();
+        // Disabling the interrupt with the Raspberry Pi Pico SDK
+        gpio_set_irq_enabled_with_callback(interruptNum, 0, false, NULL);
+    }
+
+    void delay(unsigned long ms) override {
+        sleep_ms(ms);
+    }
+
+    void delayMicroseconds(unsigned long us) override {
+        sleep_us(us / 1000);
+    }
+
+    unsigned long millis() override {
+        return to_ms_since_boot(get_absolute_time());
+    }
+
+    unsigned long micros() override {
+        return to_us_since_boot(get_absolute_time()) / 1000;
+    }
+
+    long pulseIn(uint32_t pin, uint32_t state, unsigned long timeout) override {
+        if (pin == RADIOLIB_NC) {
+            return 0;
         }
 
-        // Mode: 0 for in, 1 for out
-        // GPIO-related methods (pinMode, digitalWrite etc.) should check
-        // RADIOLIB_NC as an alias for non-connected pins
-        void pinMode(uint32_t pin, uint32_t mode) override {
-            if(pin == RADIOLIB_NC) {
-                return;
-            }
+        gpio_set_dir(pin, GPIO_IN);
 
-            gpio_init(pin);
-            gpio_set_dir(pin, mode);
-        }
+        uint32_t start = this->micros();
+        uint32_t curtick = this->micros();
+        uint32_t timeoutMicros = timeout * 1000; 
 
-        void digitalWrite(uint32_t pin, uint32_t value) override {
-            if(pin == RADIOLIB_NC) {
-                return;
-            }
-
-            gpio_put(pin, value);
-        }
-
-        uint32_t digitalRead(uint32_t pin) override {
-            if (pin == RADIOLIB_NC) {
-                return(0);
-            }
-
-            return(gpio_get(pin));
-        }
-
-        void attachInterrupt(uint32_t interruptNum, void (*interruptCb)(void), uint32_t mode) override {
-            if(interruptNum == RADIOLIB_NC) {
-                return;
-            }
-
-            // interruptNum is being input as the GPIO, will this still work?
-            // mode is either GPIO_IRQ_EDGE_FALL or GPIO_IRQ_EDGE_RISE            
-            gpio_set_irq_enabled_with_callback(interruptNum, mode, true, (gpio_irq_callback_t)interruptCb);
-        }
-
-        void detachInterrupt(uint32_t interruptNum) override {
-            if(interruptNum == RADIOLIB_NC) {
-                return;
-            }
-
-            gpio_set_irq_enabled_with_callback(interruptNum, 0, false, NULL);
-        }
-
-        void delay(unsigned long ms) override {
-            sleep_ms(ms);
-        }
-
-        void delayMicroseconds(unsigned long us) override {
-            sleep_ms(us / 1000);
-        }
-
-        unsigned long millis() override {
-            // time_us_64 returns timestamp in microseconds
-            return(time_us_64() / 1000);
-        }
-
-        unsigned long micros() override {
-            return(time_us_64());
-        }
-
-        
-        long pulseIn(uint32_t pin, uint32_t state, unsigned long timeout) override {
-            if(pin == RADIOLIB_NC) {
-                return(0);
-            }
-
-            gpio_init(pin);
-            gpio_set_dir(pin, 0); // 0 for input
-            
-            uint32_t start = time_us_64();
-
-            while(gpio_get(pin) == state) {
-                if((time_us_64() - start) > timeout) {
-                    return(0);
-                }
-            }
-            // returns in microseconds
-            return(time_us_64() - start);
-        }
-
-        void spiBegin() {
-            if(_spiHandle < 0) {
-                _spiHandle = spi_init (spi_default, _spiSpeed);
+        while (gpio_get(pin) == state) {
+            if ((this->micros() - curtick) > timeoutMicros) {
+                return 0;
             }
         }
 
-        void spiBeginTransaction() {}
+        return (this->micros() - start);
+    }
 
-        uint8_t spiTransfer(uint8_t b) {
-            char ret;
-            const uint8_t* outputBuffer = &b;
-            spi_write_blocking(spi_default, outputBuffer, 1);
-            
-            return(b);
-        }
+    void spiBegin() {
+        spi_init(_spi, _spi_speed);
+        spi_set_format(_spi, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
+    }
 
-        void spiEndTransaction() {}
+    void spiBeginTransaction() {}
 
-        void spiEnd() {
-            if(_spiHandle >= 0) {
-                spi_deinit(spi_default);
-                _spiHandle = -1;
-            }
-        }
+    void spiTransfer(uint8_t *out, size_t len, uint8_t *in) {
+        spi_write_blocking(_spi, out, len);
+        spi_read_blocking(_spi, 0, in, len);
+    }
 
+    void spiEnd() {
+        spi_deinit(_spi);
+    }
 
-    private:
-        const unsigned int _spiSpeed;
-        //const uint8_t _spiChannel; - for now just use spi0
-        int _spiHandle = -1;
-};*/
-
-// Needs to be updated with new version of RadioLib
+private:
+    const uint32_t _spi_speed;
+    spi_inst_t *_spi;
+    int _spiHandle = -1;
+};
