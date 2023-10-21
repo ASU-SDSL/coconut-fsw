35d03aa2e96541d047f41fefd646d096ceb51961
Jonathan Franco <97241584+jonathanfranco1@users.noreply.github.com>
2023-10-21 16:00:04 -0700

    Update radio.cpp

--- main/tasks/src/radio.cpp
+++ main/tasks/src/radio.cpp
@@ -15,31 +15,44 @@ void radio_task(void* unused_arg) {
 
 }
 
-int main() {
-    // Create an instance of PiPicoHal
-    PiPicoHal hal(spi0, 2000000);
+int transmit(const char* message) {
+    int state = radio.begin();
+    if (state != ERR_NONE) {
+        return state;
+    }
 
+    radio.setFrequency(433);
+    radio.setTxPower(10);
+
+    state = radio.transmit(message);
+
+    return state;
+}
+
+
+int main() {
     // Initialize the PiPicoHal
-    hal.init();
+    hal->init();
 
     // Loop forever
     while (true) {
         // Send a packet
         printf("[RFM98] Transmitting packet ... ");
-        int state = hal.transmit("Hello World!");
-        if (state == 0) {
+        int state = transmit("Hello World!");
+        if (state == ERR_NONE) {
             // The packet was successfully transmitted
             printf("success!\n");
-
-            // Wait for a second before transmitting again
-            hal.delay(1000);
         } else {
             printf("failed, code %d\n", state);
         }
+
+        // Wait for a second before transmitting again
+        sleep_ms(1000);
     }
 
     return 0;
 }
+
 
 // transmit packet using SPI? 
 // in need of transmit function using RFM98 logic? use spi_write_blocking to send data over SPI?
