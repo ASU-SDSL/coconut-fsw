949ba5722a0ef1a20a5afa09de22ceda56e2d56e
Jonathan Franco <97241584+jonathanfranco1@users.noreply.github.com>
2023-10-21 17:12:31 -0700

    Update radio.cpp

--- main/tasks/src/radio.cpp
+++ main/tasks/src/radio.cpp
@@ -1,5 +1,7 @@
 #include <RadioLib.h>
 #include "RadioLibPiHal.h"
+#define ERR_NONE 0
+//#include <SX1278.h>
 
 PiPicoHal* hal = new PiPicoHal(); // can specify the speed here as an argument if desired
 RFM98 radio = new Module(hal, 7, 17, 22, RADIOLIB_NC);
@@ -15,45 +17,21 @@ void radio_task(void* unused_arg) {
 
 }
 
-int transmit(const char* message) {
-    int state = radio.begin();
-    if (state != ERR_NONE) {
-        return state;
-    }
-
-    radio.setFrequency(433);
-    radio.setTxPower(10);
-
-    state = radio.transmit(message);
-
-    return state;
-}
-
-
 int main() {
-    // Initialize the PiPicoHal
-    hal->init();
+    //Create an instance of PiPicoHal
+    PiPicoHal hal(spi0, 2000000);
 
-    // Loop forever
-    while (true) {
-        // Send a packet
-        printf("[RFM98] Transmitting packet ... ");
-        int state = transmit("Hello World!");
-        if (state == ERR_NONE) {
-            // The packet was successfully transmitted
+    hal.init();
+
+    while(true) {
+        printf("[RFM98] Transmitting packet ...");\
+        int state = hal.transmit("Hello World!");
+        if (state == 0) {
             printf("success!\n");
+            hal.delay(1000);
         } else {
-            printf("failed, code %d\n", state);
+            printf("failed, code %d/n", state);
+
         }
-
-        // Wait for a second before transmitting again
-        sleep_ms(1000);
     }
-
-    return 0;
 }
-
-
-// transmit packet using SPI? 
-// in need of transmit function using RFM98 logic? use spi_write_blocking to send data over SPI?
-// spi_write_blocking(_spi, data, strlen(data)); ??
