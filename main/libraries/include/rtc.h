4165bfef4ddfc035f338609b87c4db7a4c19f50c
DylanL7 <dllarso4@asu.edu>
2023-10-27 14:37:16 -0700

    first rtc commit

---
+++ main/libraries/include/rtc.h
@@ -0,0 +1,3 @@
+#include "hardware/i2c.h"
+
+void set_time(uint8_t hour, uint8_t minute, uint8_t second, uint8_t, ...);
---
+++ main/libraries/src/rtc.c
@@ -0,0 +1,7 @@
+// look at eps.c
+// copy reg_write and reg_read
+#include "rtc.h"
+
+void set_time(uint8_t hour, uint8_t minute, uint8_t second, uint8_t, ...) {
+
+}
