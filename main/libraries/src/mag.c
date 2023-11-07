7f8f2b34294ad630b8bf09857746d995c9b9f4b5
iplab-flatsat lab computer <dzubey@openincident.com>
2023-11-06 19:05:48 -0700

    mag stuff

--- main/libraries/src/mag.c
+++ main/libraries/src/mag.c
@@ -1,8 +1,8 @@
 #include "mag.h"
 
 // Slave Address, depends on SDO/SA1 pin connection
-static uint8_t SAD = 0x1E; 
-//static uint8_t SAD = 0x1C;
+//static uint8_t SAD = 0x1E; 
+static uint8_t SAD = 0x1C;
 
 // Registers
 static const uint8_t WHO_AM_I = 0x0F;
@@ -27,6 +27,8 @@ static const uint8_t INT_THIS_H = 0x33;
 
 int reg_read_m(i2c_inst_t *i2c, const uint8_t addr, const uint8_t reg, uint8_t *output_buf, const uint8_t nbytes) 
 {
+    i2c_init(i2c, 100 * 1000);
+
 	int num_bytes_read = 0;
 
 	if (nbytes < 1) 
@@ -36,10 +38,10 @@ int reg_read_m(i2c_inst_t *i2c, const uint8_t addr, const uint8_t reg, uint8_t *
 	}
 
     logln_info("read");
-	i2c_write_blocking(i2c, addr, &reg, 1, true); //1000000);
+	int ret = i2c_write_timeout_us(i2c, addr, &reg, 1, true, 1000000);
 	num_bytes_read = i2c_read_timeout_us(i2c, addr, output_buf, nbytes, false, 1000000);
 
-    logln_info("here");
+    logln_info("here: %d", ret);
 
 	return num_bytes_read;
 }
@@ -76,7 +78,7 @@ int get_x_output(i2c_inst_t *i2c) { //defines function
     //low =    00000001
     //high is just the higher register, probably just the higher value
 
-    int x_out = (int) buf_low | ((int) buf_high << 8); // or logic operator, basically makes 0 or 1 = 1
+    int x_out = (int) buf_low | ((int) buf_high << 8); // or ?logic operator, basically makes 0 or 1 = 1
     //0 | 1 = 1
     //01 | 10 = 11
     // 0 | 0 = 0
