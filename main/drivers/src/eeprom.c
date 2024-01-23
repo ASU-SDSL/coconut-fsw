c6b5b07f5db7e976428ff8672d4d5e5a144e67b8
henry-lignux <59070578+henry-lignux@users.noreply.github.com>
2024-01-22 17:25:52 -0700

    Added template

--- main/drivers/src/eeprom.c
+++ main/drivers/src/eeprom.c
@@ -2,3 +2,38 @@
 
 //Slave Address
 static u_int8_t SAD = 0x50;
+
+int address_write(    i2c_inst_t *i2c,
+                const uint8_t addr,
+                const uint8_t reg,
+                uint8_t *buf,
+                const uint8_t nbytes) {
+    
+    int num_bytes_written;
+    uint8_t msg[nbytes + 1];
+
+    if (nbytes < 1) {
+        return 0;
+    }
+
+    msg[0] = reg;
+    for  (int i=0; i < nbytes; i++) {
+        msg[i + 1] = buf[i];
+    }
+    num_bytes_written = i2c_write_blocking(i2c, addr, msg, (nbytes + 1), false);
+
+    // this could be an error code as well
+    return num_bytes_written;
+}
+
+
+void write_byte(i2c_inst_t *i2c, uint8_t byte, uint8_t address) {
+
+    address_write()
+
+}
+
+
+uint8_t* read_bytes() {
+
+}
