c550361c754b1bdd204ba07c8284c21226f03182
Krishnabalaji Budhiraju <balu.budhiraju+coding@gmail.com>
2024-02-21 18:59:06 -0700

    Did a ton of work on mram drivers.

--- main/drivers/include/mram.h
+++ main/drivers/include/mram.h
@@ -3,12 +3,29 @@
 #include "log.h"
 #include <stdio.h>
 #include "pico/stdlib.h" //Probably need this
-//MAKE SURE TO UPDATE CORRESPONDING PINS ON Raspberry Pi
+
+
+//Project specific constants
+#define SPI_BUS spi0
+#define FREQ 5000
+#define PACKET_SIZE 15
+
+//PINS ON Raspberry Pi
 #define CS 1
 #define SO 2
 #define WP 3
-#define GND 4
 #define SI 5
 #define SCK 6
 #define HOLD 7
+#define GND 4
 #define PS 8
+
+//Command codes
+const uint8_t WREN = 0x06h;
+const uint8_t WRDI = 0x04h;
+const uint8_t RDSR = 0x05h;
+const uint8_t WRSR = 0x01h;
+const uint8_t READ = 0x03h;
+const uint8_t WRITE = 0x02h;
+const uint8_t SLEEP = 0xB9h;
+const uint8_t WAKE = 0xABh;
--- main/drivers/src/mram.c
+++ main/drivers/src/mram.c
@@ -1,59 +1,65 @@
 #include "mram.h"
 
-//Slave Address
-static uint8_t M24C02_ADDR = 0x50;
-static uint8_t DATA_ADDR = 0xAA; //This is where we can start writing memory
-
-//Constants
-const int packet_length = 15;
-const int max_bytes = 1028;
-
 //Global Modifiable Variables
-uint8_t cur_address = DATA_ADDR;
+uint16_t cur_addr = 0;
 
+void setup() {
+    stdio_init_all();
+    spi_init(SPI_BUS, FREQ);
+    //TODO: See if we need to do anything to change SPI Mode to 0.
 
-int address_write(i2c_inst_t *i2c, const uint8_t addr, const uint8_t reg, uint8_t* buf, const uint8_t nbytes) {
-    int num_bytes_written;
-    uint8_t msg[nbytes + 1];
+    //Setup Serial Pins for SPI
+    gpio_set_function(SO, GPIO_FUNC_SPI);
+    gpio_set_function(SCK, GPIO_FUNC_SPI);
+    gpio_set_function(SI, GPIO_FUNC_SPI);
 
-    if (nbytes < 1) {
-        return 0;
-    }
+    //Setup CS pin for SPI
+    gpio_init(CS);
+    gpio_set_dir(CS, GPIO_OUT); 
+    gpio_put(CS, 1);
 
-    msg[0] = reg;
-    for  (int i=0; i < nbytes; i++) {
-        msg[i + 1] = buf[i];
-    }
-    num_bytes_written = i2c_write_blocking(i2c, addr, msg, (nbytes + 1), false);
+    //Setup other random pins specific to our device but not necessarily required for SPI
+    gpio_set_dir(WP,GPIO_OUT);
+    gpio_put(WP, 0);
+    gpio_set_dir(HOLD, GPIO_OUT);
+    gpio_put(HOLD, 1);
 
-    // this could be an error code as well
-    return num_bytes_written;
+    //Make sure device is initially awake
+    send__simple_command(&WAKE);
+
+    //TODO: Set current address pointer using initial space in memory
 }
 
-uint8_t* read_bytes(i2c_inst_t * i2c, const uint8_t addr, const uint8_t reg, uint8_t* output_buf, const uint8_t nbytes) {
-    int num_bytes_read = 0;
-    
-    if (nbytes < 1) {
-        return 0;
-    }
-
-    i2c_write_blocking(i2c, addr, &reg, 1, true);
-    num_bytes_read = i2c_read_blocking(i2c, addr, output_buf, nbytes, false);
-    return num_bytes_read;
+void send__simple_command(uint8_t* cmd) {
+    gpio_put(CS, 0);
+    spi_write_blocking(SPI_BUS, cmd, 1);
+    gpio_put(CS, 1);
 }
 
-
-void write_byte(i2c_inst_t *i2c, uint8_t byte, uint8_t address) {
-    address_write(i2c, byte, address, 1)
+int address_write(const uint16_t addr, uint8_t* buf, const uint8_t nbytes) {
+    send__simple_command(&WREN);
+    gpio_put(CS, 0);
+    //Trying two methods, send cmd, address, and data bytes separately; Or,make big array for all of them.
+    spi_write_blocking(SPI_BUS, &WRITE, 1);
+    spi_write_blocking(SPI_BUS, &addr, 2); //TODO: Try to change uint16 to an array of uint8s or see if saying uint16 is an array of 2 uint8s works.
+    spi_write_blocking(SPI_BUS, buf, nbytes);
+    gpio_put(CS, 1);
+    send__simple_command(&WRDI);
+    //TODO: Update current address after write operation
 }
 
-
+uint8_t* read_bytes(const uint16_t addr, uint8_t* buf, const uint8_t nbytes) {
+    gpio_put(CS, 0);
+    spi_write_blocking(SPI_BUS, &READ, 1);
+    spi_write_blocking(SPI_BUS, &addr, 2); //TODO: Try to change uint16 to an array of uint8s or see if saying uint16 is an array of 2 uint8s works.
+    spi_read_blocking(SPI_BUS, buf, nbytes);
+    gpio_put(CS, 1);
+}
 
 uint8_t* write_packet(i2c_inst_t *i2c, uint8_t* buf) {
-    address_write(i2c, cur_address, DATA_ADDR, output_buf, packet_length);
+    address_write(cur_addr, buf, PACKET_SIZE);
 }
 
-uint8_t* read_packet(i2c_inst_t i2c, uint8_t* output_buf) {
-    read_bytes(i2c, cur_address, DATA_ADDR, output_buf, packet_length);
-    return 1;
+uint8_t* read_packet(i2c_inst_t i2c, uint8_t* buf) {
+    read_bytes(cur_addr, buf, PACKET_SIZE); //TODO: Make sure we start from correct address for reading most recent packets
 }
