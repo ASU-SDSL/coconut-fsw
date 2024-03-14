d6a5d50a3656fe6ebae438f1cee38f3bd59791e0
euanblackstone <blackstoneeuan@gmail.com>
2024-03-13 20:41:04 -0700

    added thread files

---
+++ main/tasks/filesystem/filesystem.h
@@ -0,0 +1,24 @@
+/*
+TODO:   sd card driver does not look finished, so finish the driver to read and write to sd card, plus any other qol functions
+        write user functions. all internal functions and structs will come out of necessity.
+
+*/
+
+
+#pragma once
+
+
+/* USER FUNCTIONS */
+
+//read file
+
+//write file
+
+//append file
+
+//create file
+
+//delete file
+
+
+/* INTERNAL FUNCTIONS */
