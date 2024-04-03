0b33cb0729159a4849fba1a112c3be341754c390
mzakocs <mzakocs@gmail.com>
2024-04-03 16:26:14 -0700

    Update cmd testing script

--- scripts/ccsds_cmd.py
+++ scripts/ccsds_cmd.py
@@ -6,15 +6,21 @@ import serial
 # ttyACM0 (usb-c) for logging messages
 ser = serial.Serial('/dev/ttyUSB0', baudrate=115200)
 
-# Create spacepacket
-spacepacket_header = SpacePacketHeader(
-    packet_type=PacketType.TC, apid=0x01, seq_count=0, data_len=3)
-header_bytes = spacepacket_header.pack()
+### Create change telem rate payload
+# spacepacket_header = SpacePacketHeader(
+#     packet_type=PacketType.TC, apid=0x01, seq_count=0, data_len=4)
+# header_bytes = spacepacket_header.pack()
+# payload = b"\x35\x2E\xF8\x53"
+# payload += header_bytes
+# payload += int.to_bytes(300, 4, byteorder='little') # ms
 
-# Create change telem rate payload
+### List STEVE task payload
+spacepacket_header = SpacePacketHeader(
+    packet_type=PacketType.TC, apid=0x04, seq_count=0, data_len=0)
+header_bytes = spacepacket_header.pack()
 payload = b"\x35\x2E\xF8\x53"
 payload += header_bytes
-payload += int.to_bytes(300, 4, byteorder='little') # ms
+
 
 # Send spacepacket uart port
 ser.write(payload)
