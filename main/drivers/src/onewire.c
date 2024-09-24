588e6b7bc9a1a9308a47dd4f832ba1a5ebf1649e
henry-lignux <59070578+henry-lignux@users.noreply.github.com>
2024-09-24 16:58:01 -0700

    added some code

--- main/drivers/src/onewire.c
+++ main/drivers/src/onewire.c
@@ -1,1 +1,32 @@
 // source file for one wire
+void config_onewire() {
+  
+}
+float getTempC() {
+    sendCommand(MATCH_ROM, CONVERT_T, !selectedPowerMode);
+    delayForConversion(selectedResolution, selectedPowerMode);
+    readScratchpad();
+    uint8_t lsb = selectedScratchpad[TEMP_LSB];
+    uint8_t msb = selectedScratchpad[TEMP_MSB];
+
+    switch (selectedResolution) {
+        case 9:
+            lsb &= 0xF8;
+            break;
+        case 10:
+            lsb &= 0xFC;
+            break;
+        case 11:
+            lsb &= 0xFE;
+            break;
+    }
+
+    uint8_t sign = msb & 0x80;
+    int16_t temp = (msb << 8) + lsb;
+
+    if (sign) {
+        temp = ((temp ^ 0xffff) + 1) * -1;
+    }
+
+    return temp / 16.0;
+}
