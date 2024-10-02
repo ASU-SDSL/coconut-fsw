935dbdac84b5cd85fd5373cf54434445f0fc1e9a
mzakocs <mzakocs@gmail.com>
2024-10-01 17:51:10 -0700

    Fixed MRAM size bug

--- main/drivers/include/mram.h
+++ main/drivers/include/mram.h
@@ -38,7 +38,7 @@
 
 void setup();
 void send_simple_command(uint8_t cmd);
-int write_bytes(const uint32_t addr, const uint8_t* buf, const uint8_t nbytes);
-int read_bytes(const uint32_t addr, uint8_t* buf, const uint8_t nbytes);
+int write_bytes(const uint32_t addr, const uint8_t* buf, const uint32_t nbytes);
+int read_bytes(const uint32_t addr, uint8_t* buf, const uint32_t nbytes);
 void mram_testing();
 
