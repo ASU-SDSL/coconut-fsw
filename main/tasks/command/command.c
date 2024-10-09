4b7edb1d541b52058bb156f59b124499e0b2671d
mzakocs <mzakocs@gmail.com>
2024-10-08 18:15:08 -0700

    Command definitions done for file transfer

--- main/tasks/command/command.c
+++ main/tasks/command/command.c
@@ -32,8 +32,8 @@ void parse_command_packet(ccsds_header_t header, uint8_t* payload_buf, uint32_t 
             break;
         case CHANGE_HEARTBEAT_TELEM_RATE:
             if (payload_size < sizeof(change_heartbeat_telem_rate_t)) break;
-            change_heartbeat_telem_rate_t* args = (change_heartbeat_telem_rate_t*)payload_buf;
-            edit_steve_job_recur_time(HEARTBEAT_JOB_NAME, args->ms);
+            change_heartbeat_telem_rate_t* heartbeat_tr_args = (change_heartbeat_telem_rate_t*)payload_buf;
+            edit_steve_job_recur_time(HEARTBEAT_JOB_NAME, heartbeat_tr_args->ms);
             break;
         case REQUEST_DOWNLINK_GROUNDNODE_DATA:
             // TODO: Implement
@@ -45,6 +45,43 @@ void parse_command_packet(ccsds_header_t header, uint8_t* payload_buf, uint32_t 
             break;
         case LIST_STEVE_TASKS:
             print_debug_exec_times();
+            break;
+        case FILE_LS:
+            if (payload_size < sizeof(file_ls_t)) break;
+            file_ls_t* ls_args = (file_ls_t*)payload_buf;
+            list_directory(ls_args->path);
+            break;
+        case FILE_MKDIR:
+            if (payload_size < sizeof(file_mkdir_t)) break;
+            file_mkdir_t* mkdir_args = (file_mkdir_t*)payload_buf;
+            make_directory(mkdir_args->path);
+            break;
+        case FILE_CAT:
+            if (payload_size < sizeof(file_cat_t)) break;
+            file_cat_t* cat_args = (file_cat_t*)payload_buf;
+            cat(cat_args->path);
+            break;
+        case FILE_DELETE: 
+            if (payload_size < sizeof(file_delete_t)) break;
+            file_delete_t* delete_args = (file_delete_t*)payload_buf;
+            delete_file(delete_args->path);
+            break;
+        case FILE_APPEND:
+            if (payload_size < sizeof(file_append_t)) break;
+            file_append_t* append_args = (file_append_t*)payload_buf;
+            if (append_args->data_len > (payload_size - sizeof(file_append_t))) { break; }
+            write_file(append_args->path, append_args->data, append_args->data_len, true);
+            break;
+        case FILE_TOUCH:
+            if (payload_size < sizeof(file_touch_t)) break;
+            file_touch_t* touch_args = (file_touch_t*)payload_buf;
+            touch(touch_args->path);
+            break;
+        case FILE_MKFS:
+            if (payload_size < sizeof(file_mkfs_t)) break;
+            file_mkfs_t* mkfs_args = (file_mkfs_t*)payload_buf;
+            if (mkfs_args->confirm != 1) { break; }
+            make_filesystem();
             break;
         default:
             logln_error("Received command with unknown APID: %hu", header.apid);
