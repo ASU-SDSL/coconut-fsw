044daea3a5bb893dc4444b7b73d37f0e27fe651c
mzakocs <mzakocs@gmail.com>
2024-10-19 16:09:07 -0700

    Turn on CUSTOM_MUTATOR_ONLY again

--- main/test/fuzz/fuzz.sh
+++ main/test/fuzz/fuzz.sh
@@ -4,6 +4,6 @@
 # export AFL_DEBUG=1
 export AFL_SKIP_CPUFREQ=1
 export ASAN_OPTIONS=detect_leaks=0,abort_on_error=1,symbolize=0
-# export AFL_CUSTOM_MUTATOR_ONLY=1
+export AFL_CUSTOM_MUTATOR_ONLY=1
 export AFL_CUSTOM_MUTATOR_LIBRARY=$(pwd)/build/Fuzzer/lib/AFLplusplus-protobuf-mutator/coconut/libcommand_proto_mutator.so
 afl-fuzz -i main/test/fuzz/corpus -o output -- ./build/Fuzzer/main/test/fuzz/command_fuzzer @@
