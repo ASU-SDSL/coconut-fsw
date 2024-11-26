ed8e3a99d459c50f33a4c74ee447e26bb982bf8e
DylanL7 <dllarso4@asu.edu>
2024-11-26 13:38:22 -0700

    init batt gauge

---
+++ main/drivers/src/max17048.c
@@ -0,0 +1,14 @@
+#include "max17048.h"
+
+#define MAX17048_I2C_ADDR 0x36
+#define REG_VCELL 0x02
+#define REG_SOC 0x04
+#define REG_MODE 0x06
+#define REG_HIBRT 0x0A
+#define REG_CONFIG 0x0C
+#define REG_STATUS 0x1A
+#define REG_CMD 0xFE
+
+/*
+* 
+*/
