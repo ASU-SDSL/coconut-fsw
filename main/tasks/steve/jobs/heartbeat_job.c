144215a15f1cff0e2d16fe13b4170c366c43ab5f
DylanL7 <dllarso4@asu.edu>
2025-02-09 20:23:59 -0700

    tlm storage

--- main/tasks/steve/jobs/heartbeat_job.c
+++ main/tasks/steve/jobs/heartbeat_job.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdio.h>
 
 #include "FreeRTOS.h"
 #include "queue.h"
@@ -14,6 +15,67 @@
 #include "heartbeat_job.h"
 #include "radio.h"
 #include "max17048.h"
+#include "filesystem.h"
+
+// Size (bytes) that a heartbeat telemetry file can be until a new one should be used
+// If writing a new telemetry entry causes the file to go over this amount, a new file is made
+#define MAX_HB_TLM_FILE_SIZE 200
+
+/* 
+*  Keep a /tlm directory where:
+*  - /tlm/file_counter.bin file contains the index of the next file to be created - 2 bytes
+*  - /tlm/0.bin, /tlm/1.bin, ... /tlm/65535 files contain the telemetry data, most recent of which is the number.bin contained in the file_counter.txt file
+*/
+void log_heartbeat_tlm(heartbeat_telemetry_t payload) {
+
+    // See if tlm directory exists
+    if (!dir_exists("/tlm")) {
+        make_dir("/tlm");
+    }
+    // See if f_count.bin exists, if not create it and start the index at 0
+    if (!file_exists("/tlm/f_count.bin")) {
+        // If the directory exists but the file counter file doesn't, create it and write a 0 to it
+        //touch("/tlm/f_count.bin");
+
+        char data[] = {0, 0};
+        write_file("/tlm/f_count.bin", data, 2, false); // create and write 0 to the file_counter.txt file
+
+        logln_info("TLM dir:");
+        list_dir("/tlm");
+    }
+
+    // Read the file counter
+    char file_counter_buf[2];
+    size_t bytes_read = read_file("/tlm/f_count.bin", file_counter_buf, sizeof(file_counter_buf));
+    if (bytes_read != 2) {
+        logln_error("Error reading f_count.bin");
+        return;
+    }
+
+    uint16_t file_count = (file_counter_buf[0] << 8) | file_counter_buf[1];
+    logln_info("Heartbeat tlm file count: %d", file_count);
+    
+    // Write to this indexed file
+    char filename[20];
+    snprintf(filename, sizeof(filename), "%d.bin", file_count);
+    logln_info("Writing to file: %s", filename);
+    write_file(filename, (char*)&payload, sizeof(payload), true); // append - this will eaither make the file if it does not exist or append to it if it does
+
+    // Check file size to see if it is time to make a new file. If writing another hb to the file makes it go over the limit, 
+    FILINFO file_info;
+    stat(filename, &file_info);
+    if (file_info.fsize + sizeof(heartbeat_telemetry_t) > MAX_HB_TLM_FILE_SIZE) {
+        // Increase f_count.bin by 1 for next time
+        file_count += 1;
+        char new_file_counter_buf[2] = {(file_count >> 8) & 0xFF, file_count & 0xFF};
+        write_file("/tlm/f_count.bin", new_file_counter_buf, 2, false); // overwrite the file counter with the new value
+        logln_info("New tlm file should be created");
+    }
+
+    logln_info("\nTLM DIR:");
+    list_dir("/tlm");
+    
+}
 
 void heartbeat_telemetry_job(void* unused) {
     // Create heartbeat struct
@@ -77,10 +139,10 @@ void heartbeat_telemetry_job(void* unused) {
     if(!getPower_raw(i2c, INA0_ADDR, &ina0buf)) payload.ina0_power = ina0buf;
     else payload.ina0_power = UINT16_MAX; 
 
-    logln_info("INA0 shunt: %ld", payload.ina0_shunt);
+    /*logln_info("INA0 shunt: %ld", payload.ina0_shunt);
     logln_info("INA0 vbus: %ld", payload.ina0_vbus);
     logln_info("INA0 current: %ld", payload.ina0_current);
-    logln_info("INA0 power: %ld", payload.ina0_power);
+    logln_info("INA0 power: %ld", payload.ina0_power);*/
 
     // ina1 data
     uint16_t ina1buf;
@@ -160,6 +222,7 @@ void heartbeat_telemetry_job(void* unused) {
     // Send it
     logln_info("Telem size: %d", sizeof(payload));
     send_telemetry(HEARTBEAT, (char*)&payload, sizeof(payload));
+    log_heartbeat_tlm(payload);
 
     iteration_counter += 1;
     logln_info("Heartbeat %ld - uptime: %d", iteration_counter, (uint32_t)get_uptime());
