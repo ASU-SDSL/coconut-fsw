9da22c733f569a953ec8d7cdc8d6de4a565453d4
KB-The-Developer <86029905+KB-The-Developer@users.noreply.github.com>
2025-04-01 18:09:16 -0700

    Update miscallaneous_jobs.c

--- main/tasks/steve/jobs/miscallaneous_jobs.c
+++ main/tasks/steve/jobs/miscallaneous_jobs.c
@@ -1,1 +1,21 @@
+#include "hardware/gpio.h"
+#include "steve.h"
 
+#define POWER_PIN 20
+
+void buzzer_init() {
+  gpio_set_dir(POWER_PIN, GPIO_OUT);
+}
+
+void buzzer_beep_job() {
+  schedule_delayed_job_ms("Buzzer beep on", &beep_on, 10);
+  schedule_delayed_job_ms("Buzzer beep off", &beep_on, 10);
+}
+
+static void beep_on() {
+  gpio_put(POWER_PIN, 1);
+} 
+
+static void beep_off() {
+  gpio_put(POWER_PIN, 0);
+}
