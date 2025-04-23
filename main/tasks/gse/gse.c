982cd6be766ca86bcda8ea5c11006c1040d72b8a
LabComputerIPL <LabComputerIPL@fakeemail.com>
2025-04-22 19:23:08 -0700

    Buzzer working yay

--- main/tasks/gse/gse.c
+++ main/tasks/gse/gse.c
@@ -10,7 +10,6 @@
 #include "ina219.h"
 #include "log.h"
 #include "gse.h"
-#include "miscellaneous_jobs.h"
 
 void gse_queue_message(char* buffer, size_t size) {
     // write to picosdk usb uart interface
@@ -20,11 +19,6 @@ void gse_queue_message(char* buffer, size_t size) {
 void gse_task(void *pvParameters) {
     // Initialize USB UART
     stdio_init_all();
-
-    for(int i = 0; i < 100; i++){
-        buzzer_beep_job(); 
-        vTaskDelay(pdMS_TO_TICKS(500)); 
-    }
 
     // Start listening for USB UART bytes
     while (true) {
--- main/tasks/steve/jobs/miscellaneous_jobs.c
+++ main/tasks/steve/jobs/miscellaneous_jobs.c
@@ -1,22 +1,25 @@
 #include "FreeRTOS.h"
 
 #include "hardware/gpio.h"
+#include "pico/stdlib.h"
+#include "hardware/clocks.h"
+#include "hardware/pwm.h"
 #include "steve.h"
 #include "log.h"
 
 #define SYS_CLOCK_FREQ
 #define BUZZER_PIN 4
 #define BUZZER_FREQ_HZ 1000
-#define BUZZER_POWER 50 //Basically this is the duty cycle which also corresponds to amount of power
+#define BUZZER_POWER 0.9 //Basically this is the duty cycle which also corresponds to amount of power
 
 void buzzer_beep_job() {
   //Initialize pwm stuff
   logln_info("Beep!"); 
-  gpio_set_function(BUZZER_PIN,  GPIO_FUNC_PWN);
+  gpio_set_function(BUZZER_PIN,  GPIO_FUNC_PWM);
   uint slice_num = pwm_gpio_to_slice_num(BUZZER_PIN);
-  int wrap = (int) (clock_get_hz(clk_sys) / BUZZER_FREQ_HZ);
+  uint16_t wrap = (uint16_t) (clock_get_hz(clk_sys) / BUZZER_FREQ_HZ);
   pwm_set_wrap(slice_num, wrap);
-  pwm_set_chan_level(slice_num, PWM_CHAN_A, BUZZER_POWER);
+  pwm_set_chan_level(slice_num, PWM_CHAN_A, (uint16_t)(BUZZER_POWER * wrap));
 
   for(int i = 0; i < 3; i++) {
     pwm_set_enabled(slice_num, true);
