170a5d6481cfd7f8865a5b28c2264e54d4565381
Tyler Nielsen <tylernielsenn@gmail.com>
2025-09-07 05:01:24 +0000

    queue based intertask watchdog draft

--- main/tasks/watchdog/watchdog.c
+++ main/tasks/watchdog/watchdog.c
@@ -3,9 +3,18 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "log.h"
+#include <stdlib.h>
 
 #define WDI_PIN 21 // from FCR schematic
 
+#define WATCHDOG_MAX_QUEUE_ITEMS 16
+#define WATCHDOG_INTERTASK_CHECK_PERIOD_MS 1000 * 60 // every minute 
+#define WATCHDOG_CONNECTED_TASKS 2 
+#define WATCHDOG_CHECK_DELAY_MS 500
+
+static QueueHandle_t watchdog_queue;
+static bool heartbeats[WATCHDOG_CONNECTED_TASKS]; 
+static uint32_t last_check = 0; 
 
 void watchdog_freeze() {
     // Notify main loop in watchdog_task() to freeze
@@ -13,6 +22,12 @@ void watchdog_freeze() {
 }
 
 void watchdog_task(void *pvParameters) {
+
+    // set up watchdog queue
+    watchdog_queue = xQueueCreate(WATCHDOG_MAX_QUEUE_ITEMS, sizeof(uint8_t));
+
+    // update last check 
+    last_check = to_ms_since_boot(get_absolute_time()); 
 
     // Initialize the watchdog GPIO and set it high
     gpio_init(WDI_PIN);
@@ -29,10 +44,37 @@ void watchdog_task(void *pvParameters) {
             while(1) {}
         }
 
+        // receive intertask heartbeats from queue
+        uint8_t heartbeat_index; 
+        while(xQueueReceive(watchdog_queue, &heartbeat_index, 0) == pdPASS){
+            heartbeats[heartbeat_index] = true; 
+        }
+
+        if(abs((long long)(to_ms_since_boot(get_absolute_time()) - last_check)) > WATCHDOG_INTERTASK_CHECK_PERIOD_MS){
+            // check heartbeats 
+            for(size_t i = 0; i < WATCHDOG_CONNECTED_TASKS; i++){
+                // if a heartbeat hasn't toggled since last check 
+                if(heartbeats[i] == false){
+                    watchdog_freeze(); 
+                    break; 
+                }
+                // reset heartbeat
+                heartbeats[i] = false; 
+            }
+        }
+
         state = !state; // Flip state to toggle on and off
         gpio_put(WDI_PIN, state);
         vTaskDelay(500);
 
     }
 
+}
+
+void watchdog_kick(uint8_t id){
+    while(!watchdog_queue)
+    {
+        vTaskDelay(WATCHDOG_CHECK_DELAY_MS / portTICK_PERIOD_MS); 
+    }
+    xQueueSendToBack(watchdog_queue, &id, portMAX_DELAY); 
 }
