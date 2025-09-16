07d4e28239d14a11cab82661c924f62bc8644f08
SnailDragon <tylernielsenn@gmail.com>
2025-09-15 20:55:42 -0700

    fixed comment

--- freertos/FreeRTOSConfig.h
+++ freertos/FreeRTOSConfig.h
@@ -38,7 +38,7 @@
 #define configSUPPORT_STATIC_ALLOCATION         0
 #define configSUPPORT_DYNAMIC_ALLOCATION        1           // Get FreeRTOS to allocation task memory
 #define configAPPLICATION_ALLOCATED_HEAP        0
-#define configTOTAL_HEAP_SIZE                   200000      // 230 KB 
+#define configTOTAL_HEAP_SIZE                   200000      // 200 KB 
 
 /* Hook function related definitions. */
 #define configUSE_IDLE_HOOK                     0
