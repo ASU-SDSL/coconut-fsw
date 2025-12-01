28368fdebd07e3cebb19fd2af25e03d9c65d71a3
SnailDragon <tylernielsenn@gmail.com>
2025-12-01 04:15:33 -0700

    added naive valid flag to antenna deploy

--- main/tasks/steve/jobs/antenna_deploy_job.c
+++ main/tasks/steve/jobs/antenna_deploy_job.c
@@ -1,6 +1,7 @@
 #include "vega_ant.h"
 #include "log.h"
 #include "steve.h"
+#include "radio.h"
 
 void deploy_antenna_job() {
     logln_info("Deploying antenna...");
@@ -16,9 +17,11 @@ void deploy_antenna_job() {
     uint8_t antenna_switch_status = 0; 
     status = vega_ant_status(i2c, &antenna_switch_status); 
     if (status != 0 || antenna_switch_status != 0) {
-        logln_error("Antenna switch status %d, retrying in 10 min");
+        logln_error("Antenna switch status %d, retrying in 10 min", antenna_switch_status);
         schedule_delayed_job_mins("DEPLOY_ANTENNA", &deploy_antenna_job, 10);
     } else {
         logln_info("Deploy antenna task complete");
     }
+
+    radio_flag_valid_packet(); // if the antenna just deployed radio can start working
 }
